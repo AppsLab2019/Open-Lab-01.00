b907abbcc1f11c61de35efb256691e91e4ea2653
Peter Milovcik <peter.milovcik@gmail.com>
2019-10-21 08:18:45 +0200

    Initial

---
+++ Open-Lab-01.00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
